d000288e2b58a481cc5609c01062b979a8598c9a
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-16 13:21:37 -0700

    start to julie class

---
+++ Julie.java
@@ -0,0 +1,46 @@
+import processing.core.PImage;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Julie extends MoveAbstract{
+
+    private final int resourceLimit;
+
+    public Julie(String id, Point position, List<PImage> images, int resourceLimit, int resourceCount,
+                        int actionPeriod, int animationPeriod)
+    {
+        super(id, position, images, actionPeriod, animationPeriod);
+        this.resourceLimit = resourceLimit;
+
+    }
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler)
+    {
+        Optional<Entity> fullTarget = world.findNearest(getPosition(),
+                    Blacksmith.class);
+
+        if (fullTarget.isPresent() &&
+                moveTo(world, fullTarget.get(), scheduler))
+        { transform(world, scheduler, imageStore); }
+        else {
+            scheduler.scheduleEvent(this,
+                    createActivity(world, imageStore),
+                    getActionPeriod());
+        }
+    }
+
+    private void transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore)
+    {
+        MinerNotFull miner = Factory.createMinerNotFull(getId(), resourceLimit,
+                getPosition(), getActionPeriod(), getAnimationPeriod(),
+                getImages());
+
+        world.removeEntity(this);
+        scheduler.unscheduleAllEvents(this);
+
+        world.addEntity(miner);
+        miner.scheduleActions(scheduler, world, imageStore);
+    }
+}
+
+
