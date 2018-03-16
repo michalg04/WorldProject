1020f02da35110ac6afceb2fc810e88cffcf2070
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-16 13:36:18 -0700

    start to rat class

---
+++ Rat.java
@@ -0,0 +1,93 @@
+import processing.core.PImage;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class Rat extends MoveAbstract{
+    private static final String QUAKE_KEY = "quake";
+
+    public Rat(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod)
+    {
+        super(id, position, images, actionPeriod, animationPeriod);
+    }
+
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler)
+    {
+        Optional<Entity> blobTarget = world.findNearest(getPosition(), Vein.class);
+        long nextPeriod = getActionPeriod();
+
+        if (blobTarget.isPresent())
+        {
+            Point tgtPos = blobTarget.get().getPosition();
+
+            if (moveTo(world, blobTarget.get(), scheduler))
+            {
+                Quake quake = Factory.createQuake(tgtPos,
+                        imageStore.getImageList(QUAKE_KEY));
+
+                world.addEntity(quake);
+                nextPeriod += getActionPeriod();
+                quake.scheduleActions(scheduler, world, imageStore);
+            }
+        }
+
+        scheduler.scheduleEvent(this,
+                createActivity(world, imageStore),
+                nextPeriod);
+    }
+
+    public Point nextPosition(WorldModel world, Point destPos)
+    { /*
+        int horiz = Integer.signum(destPos.x - getPosition().x);
+        Point newPos = new Point(getPosition().x + horiz, getPosition().y);
+
+        Optional<Entity> occupant = world.getOccupant(newPos);
+
+        if (horiz == 0 ||
+                (occupant.isPresent() && !(occupant.get() instanceof Ore)))
+        {
+            int vert = Integer.signum(destPos.y - getPosition().y);
+            newPos = new Point(getPosition().x, getPosition().y + vert);
+            occupant = world.getOccupant(newPos);
+
+            if (vert == 0 ||
+                    (occupant.isPresent() && !(occupant.get() instanceof Ore)))
+            {
+                newPos = getPosition();
+            }
+        }
+        return newPos;
+        */
+        int horiz = Integer.signum(destPos.x - getPosition().x);
+        Point newPos = new Point(getPosition().x + horiz, getPosition().y);
+
+        Optional<Entity> occupant = world.getOccupant(newPos);
+        PathingStrategy strategy = new AStarPathingStrategy();
+        Predicate<Point> canPassThrough = p -> ((!world.isOccupied(p) ||
+                (occupant.isPresent() && occupant.get() instanceof Ore)) && world.withinBounds(p));
+        BiPredicate<Point, Point> withinReach = (Point p1, Point p2) -> adjacent(p1, p2);
+        List<Point> path = strategy.computePath(getPosition(),
+                destPos, canPassThrough, withinReach, PathingStrategy.CARDINAL_NEIGHBORS);
+
+        if (path == null)
+            return getPosition();
+        if (path.size() == 0)
+            return getPosition();
+        return path.get(0);
+
+
+    }
+
+    public boolean moveTo(WorldModel world, Entity target, EventScheduler scheduler) {
+        if (super.moveTo(world, target, scheduler)) {
+            world.removeEntity(target);
+            scheduler.unscheduleAllEvents(target);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
