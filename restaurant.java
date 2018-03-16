ab339c4442827d9c46333f5c997436098e9e1c18
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-16 13:22:54 -0700

    start to restaurant

---
+++ restaurant.java
@@ -0,0 +1,12 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class restaurant extends EntityAbstract {
+
+    public restaurant(String id, Point position, List<PImage> images)
+    {
+        super(id, position, images);
+    }
+
+}
