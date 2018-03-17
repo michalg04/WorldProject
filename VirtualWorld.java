41a55362a1666315c0695192f58b3041a1ce399f
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-17 12:21:37 -0700

    reformat

--- VirtualWorld.java
+++ VirtualWorld.java
@@ -1,221 +1,191 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 import processing.core.*;
+
 public final class VirtualWorld
-   extends PApplet
-{
-   private static final int TIMER_ACTION_PERIOD = 100;
+        extends PApplet {
+    private static final int TIMER_ACTION_PERIOD = 100;
 
-   private static final int VIEW_WIDTH = 640;
-   private static final int VIEW_HEIGHT = 480;
-   private static final int TILE_WIDTH = 32;
-   private static final int TILE_HEIGHT = 32;
-   private static final int WORLD_WIDTH_SCALE = 2;
-   private static final int WORLD_HEIGHT_SCALE = 2;
+    private static final int VIEW_WIDTH = 640;
+    private static final int VIEW_HEIGHT = 480;
+    private static final int TILE_WIDTH = 32;
+    private static final int TILE_HEIGHT = 32;
+    private static final int WORLD_WIDTH_SCALE = 2;
+    private static final int WORLD_HEIGHT_SCALE = 2;
 
-   private static final int VIEW_COLS = VIEW_WIDTH / TILE_WIDTH;
-   private static final int VIEW_ROWS = VIEW_HEIGHT / TILE_HEIGHT;
-   private static final int WORLD_COLS = VIEW_COLS * WORLD_WIDTH_SCALE;
-   private static final int WORLD_ROWS = VIEW_ROWS * WORLD_HEIGHT_SCALE;
+    private static final int VIEW_COLS = VIEW_WIDTH / TILE_WIDTH;
+    private static final int VIEW_ROWS = VIEW_HEIGHT / TILE_HEIGHT;
+    private static final int WORLD_COLS = VIEW_COLS * WORLD_WIDTH_SCALE;
+    private static final int WORLD_ROWS = VIEW_ROWS * WORLD_HEIGHT_SCALE;
 
-   private static final String IMAGE_LIST_FILE_NAME = "imagelist";
-   private static final String DEFAULT_IMAGE_NAME = "background_default";
-   private static final int DEFAULT_IMAGE_COLOR = 0x808080;
+    private static final String IMAGE_LIST_FILE_NAME = "imagelist";
+    private static final String DEFAULT_IMAGE_NAME = "background_default";
+    private static final int DEFAULT_IMAGE_COLOR = 0x808080;
 
-   private static final String LOAD_FILE_NAME = "gaia.sav";
+    private static final String LOAD_FILE_NAME = "gaia.sav";
 
-   private static final String FAST_FLAG = "-fast";
-   private static final String FASTER_FLAG = "-faster";
-   private static final String FASTEST_FLAG = "-fastest";
-   private static final double FAST_SCALE = 0.5;
-   private static final double FASTER_SCALE = 0.25;
-   private static final double FASTEST_SCALE = 0.10;
+    private static final String FAST_FLAG = "-fast";
+    private static final String FASTER_FLAG = "-faster";
+    private static final String FASTEST_FLAG = "-fastest";
+    private static final double FAST_SCALE = 0.5;
+    private static final double FASTER_SCALE = 0.25;
+    private static final double FASTEST_SCALE = 0.10;
 
-   private static double timeScale = 1.0;
+    private static double timeScale = 1.0;
 
-   private ImageStore imageStore;
-   private WorldModel world;
-   private WorldView view;
-   private EventScheduler scheduler;
+    private ImageStore imageStore;
+    private WorldModel world;
+    private WorldView view;
+    private EventScheduler scheduler;
 
-   private long next_time;
+    private long next_time;
 
-   public void settings()
-   {
-      size(VIEW_WIDTH, VIEW_HEIGHT);
-   }
+    public void settings() {
+        size(VIEW_WIDTH, VIEW_HEIGHT);
+    }
 
-   /*
-      Processing entry point for "sketch" setup.
-   */
-   public void setup()
-   {
-      this.imageStore = new ImageStore(
-         createImageColored(TILE_WIDTH, TILE_HEIGHT, DEFAULT_IMAGE_COLOR));
-      this.world = new WorldModel(WORLD_ROWS, WORLD_COLS,
-         createDefaultBackground(imageStore));
-      this.view = new WorldView(VIEW_ROWS, VIEW_COLS, this, world,
-         TILE_WIDTH, TILE_HEIGHT);
-      this.scheduler = new EventScheduler(timeScale);
+    /*
+       Processing entry point for "sketch" setup.
+    */
+    public void setup() {
+        this.imageStore = new ImageStore(
+                createImageColored(TILE_WIDTH, TILE_HEIGHT, DEFAULT_IMAGE_COLOR));
+        this.world = new WorldModel(WORLD_ROWS, WORLD_COLS,
+                createDefaultBackground(imageStore));
+        this.view = new WorldView(VIEW_ROWS, VIEW_COLS, this, world,
+                TILE_WIDTH, TILE_HEIGHT);
+        this.scheduler = new EventScheduler(timeScale);
 
-      loadImages(IMAGE_LIST_FILE_NAME, imageStore, this);
-      loadWorld(world, LOAD_FILE_NAME, imageStore);
+        loadImages(IMAGE_LIST_FILE_NAME, imageStore, this);
+        loadWorld(world, LOAD_FILE_NAME, imageStore);
 
-      scheduleActions(world, scheduler, imageStore);
+        scheduleActions(world, scheduler, imageStore);
 
-      next_time = System.currentTimeMillis() + TIMER_ACTION_PERIOD;
-   }
+        next_time = System.currentTimeMillis() + TIMER_ACTION_PERIOD;
+    }
 
-   public void draw()
-   {
-      long time = System.currentTimeMillis();
-      if (time >= next_time)
-      {
-         this.scheduler.updateOnTime(time);
-         next_time = time + TIMER_ACTION_PERIOD;
-      }
+    public void draw() {
+        long time = System.currentTimeMillis();
+        if (time >= next_time) {
+            this.scheduler.updateOnTime(time);
+            next_time = time + TIMER_ACTION_PERIOD;
+        }
 
-      view.drawViewport();
-   }
+        view.drawViewport();
+    }
 
-   public void keyPressed()
-   {
-      if (key == CODED)
-      {
-         int dx = 0;
-         int dy = 0;
+    public void keyPressed() {
+        if (key == CODED) {
+            int dx = 0;
+            int dy = 0;
 
-         switch (keyCode)
-         {
-            case UP:
-               dy = -1;
-               break;
-            case DOWN:
-               dy = 1;
-               break;
-            case LEFT:
-               dx = -1;
-               break;
-            case RIGHT:
-               dx = 1;
-               break;
-         }
-         view.shiftView(dx, dy);
-      }
-   }
+            switch (keyCode) {
+                case UP:
+                    dy = -1;
+                    break;
+                case DOWN:
+                    dy = 1;
+                    break;
+                case LEFT:
+                    dx = -1;
+                    break;
+                case RIGHT:
+                    dx = 1;
+                    break;
+            }
+            view.shiftView(dx, dy);
+        }
+    }
 
-   private static Background createDefaultBackground(ImageStore imageStore)
-   {
-      return new Background(DEFAULT_IMAGE_NAME, imageStore.getImageList(DEFAULT_IMAGE_NAME));
-   }
+    private static Background createDefaultBackground(ImageStore imageStore) {
+        return new Background(DEFAULT_IMAGE_NAME, imageStore.getImageList(DEFAULT_IMAGE_NAME));
+    }
 
-   private static PImage createImageColored(int width, int height, int color)
-   {
-      PImage img = new PImage(width, height, RGB);
-      img.loadPixels();
-      for (int i = 0; i < img.pixels.length; i++)
-      {
-         img.pixels[i] = color;
-      }
-      img.updatePixels();
-      return img;
-   }
+    private static PImage createImageColored(int width, int height, int color) {
+        PImage img = new PImage(width, height, RGB);
+        img.loadPixels();
+        for (int i = 0; i < img.pixels.length; i++) {
+            img.pixels[i] = color;
+        }
+        img.updatePixels();
+        return img;
+    }
 
-   private static void loadImages(String filename, ImageStore imageStore,
-      PApplet screen)
-   {
-      try
-      {
-         Scanner in = new Scanner(new File(filename));
-         Load.loadImages(in, imageStore, screen);
-      }
-      catch (FileNotFoundException e)
-      {
-         System.err.println(e.getMessage());
-      }
-   }
+    private static void loadImages(String filename, ImageStore imageStore, PApplet screen) {
+        try {
+            Scanner in = new Scanner(new File(filename));
+            Load.loadImages(in, imageStore, screen);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
-   private static void loadWorld(WorldModel world, String filename,
-      ImageStore imageStore)
-   {
-      try
-      {
-         Scanner in = new Scanner(new File(filename));
-         Load.load(in, world, imageStore);
-      }
-      catch (FileNotFoundException e)
-      {
-         System.err.println(e.getMessage());
-      }
-   }
+    private static void loadWorld(WorldModel world, String filename, ImageStore imageStore) {
+        try {
+            Scanner in = new Scanner(new File(filename));
+            Load.load(in, world, imageStore);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
-   private static void scheduleActions(WorldModel world,
-      EventScheduler scheduler, ImageStore imageStore)
-   {
-      for (Entity entity : world.getEntities())
-      {
-         if (entity instanceof ActiveEntity) {
-            ((ActiveEntity) entity).scheduleActions(scheduler, world, imageStore);
-         }
-      }
-   }
+    private static void scheduleActions(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
+        for (Entity entity : world.getEntities()) {
+            if (entity instanceof ActiveEntity) {
+                ((ActiveEntity) entity).scheduleActions(scheduler, world, imageStore);
+            }
+        }
+    }
 
-   private static void parseCommandLine(String [] args)
-   {
-      for (String arg : args)
-      {
-         switch (arg)
-         {
-            case FAST_FLAG:
-               timeScale = Math.min(FAST_SCALE, timeScale);
-               break;
-            case FASTER_FLAG:
-               timeScale = Math.min(FASTER_SCALE, timeScale);
-               break;
-            case FASTEST_FLAG:
-               timeScale = Math.min(FASTEST_SCALE, timeScale);
-               break;
-         }
-      }
-   }
+    private static void parseCommandLine(String[] args) {
+        for (String arg : args) {
+            switch (arg) {
+                case FAST_FLAG:
+                    timeScale = Math.min(FAST_SCALE, timeScale);
+                    break;
+                case FASTER_FLAG:
+                    timeScale = Math.min(FASTER_SCALE, timeScale);
+                    break;
+                case FASTEST_FLAG:
+                    timeScale = Math.min(FASTEST_SCALE, timeScale);
+                    break;
+            }
+        }
+    }
 
-   public static void main(String [] args)
-   {
-      parseCommandLine(args);
-      PApplet.main(VirtualWorld.class);
-   }
-   
-    public void mousePressed()
-   {
-      Point point = view.colRowToPoint(mouseX/TILE_WIDTH, mouseY/TILE_HEIGHT);
+    public static void main(String[] args) {
+        parseCommandLine(args);
+        PApplet.main(VirtualWorld.class);
+    }
 
-       // Making sure I don't spawn a dragon on top of an existing entity.
-       for (Entity entity : world.getEntities())
-       {
-           if (entity.getPosition().equals(point))
-           {
-               System.out.println("Entity");
-               return;
-           }
-       }
+    public void mousePressed() {
+        Point point = view.colRowToPoint(mouseX / TILE_WIDTH, mouseY / TILE_HEIGHT);
 
-       // Add therestaurant
-       Restaurant restaurant = Factory.createRestaurant("RESTAURANT_" + point.x + "_" + point.y,
-               point, imageStore.getImageList("restaurant"));
+        // Making sure I don't spawn a dragon on top of an existing entity.
+        for (Entity entity : world.getEntities()) {
+            if (entity.getPosition().equals(point)) {
+                System.out.println("Entity");
+                return;
+            }
+        }
 
-       world.addEntity(restaurant);
-       restaurant.scheduleActions(scheduler, world, imageStore);
+        // Add therestaurant
+        Restaurant restaurant = Factory.createRestaurant("RESTAURANT_" + point.x + "_" + point.y,
+                point, imageStore.getImageList("restaurant"));
 
-       // Make flowers
-       List<Point> neighbors = world.getNeighborPoints(point, 2);
-       Random rand = new Random();
-       for (Point p : neighbors)
-       {
-           int dist = point.distanceSquared(p);
-           if (dist < rand.nextInt(6) + 1)
-              world.setBackgroundImage(imageStore.getImageList("flower"), p);
-       }
-   }
+        world.addEntity(restaurant);
+        restaurant.scheduleActions(scheduler, world, imageStore);
+
+        // Make flowers
+        List<Point> neighbors = world.getNeighborPoints(point, 2);
+        Random rand = new Random();
+        for (Point p : neighbors) {
+            int dist = point.distanceSquared(p);
+            if (dist < rand.nextInt(6) + 1)
+                world.setBackgroundImage(imageStore.getImageList("flower"), p);
+        }
+    }
 
 }
