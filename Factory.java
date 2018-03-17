949c3a4b5635e3d68969ab91a9df74aafa9a7f54
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-17 13:05:22 -0700

    reformat

--- Factory.java
+++ Factory.java
@@ -10,82 +10,69 @@ public class Factory {
 
 
     public static Blacksmith createBlacksmith(String id, Point position,
-                                          List<PImage> images)
-    {
+                                              List<PImage> images) {
         return new Blacksmith(id, position, images);
     }
-    
+
     public static Restaurant createRestaurant(String id, Point position,
-                                          List<PImage> images)
-    {
+                                              List<PImage> images) {
         return new Restaurant(id, position, images);
     }
 
     public static MinerFull createMinerFull(String id, int resourceLimit,
-                                         Point position, int actionPeriod, int animationPeriod,
-                                         List<PImage> images)
-    {
+                                            Point position, int actionPeriod, int animationPeriod,
+                                            List<PImage> images) {
         return new MinerFull(id, position, images,
                 resourceLimit, resourceLimit, actionPeriod, animationPeriod);
     }
 
     public static MinerNotFull createMinerNotFull(String id, int resourceLimit,
-                                            Point position, int actionPeriod, int animationPeriod,
-                                            List<PImage> images)
-    {
+                                                  Point position, int actionPeriod, int animationPeriod,
+                                                  List<PImage> images) {
         return new MinerNotFull(id, position, images, resourceLimit, 0, actionPeriod, animationPeriod);
     }
-    
+
     public static Julie createJulie(String id, int resourceLimit,
-                                            Point position, int actionPeriod, int animationPeriod,
-                                            List<PImage> images)
-    {
+                                    Point position, int actionPeriod, int animationPeriod,
+                                    List<PImage> images) {
         return new Julie(id, position, images, resourceLimit, 0, actionPeriod, animationPeriod);
     }
 
     public static Obstacle createObstacle(String id, Point position,
-                                        List<PImage> images)
-    {
+                                          List<PImage> images) {
         return new Obstacle(id, position, images);
     }
 
     public static Ore createOre(String id, Point position, int actionPeriod,
-                                   List<PImage> images)
-    {
+                                List<PImage> images) {
         return new Ore(id, position, images, actionPeriod);
     }
-    
+
     public static Cookie createCookie(String id, Point position, int actionPeriod,
-                                   List<PImage> images)
-    {
+                                      List<PImage> images) {
         return new Cookie(id, position, images, actionPeriod);
     }
-    
-    public static Flower createFlower(String id, Point position, List<PImage> images)
-    {
+
+    public static Flower createFlower(String id, Point position, List<PImage> images) {
         return new Flower(id, position, images);
     }
 
     public static OreBlob createOreBlob(String id, Point position,
-                                       int actionPeriod, int animationPeriod, List<PImage> images)
-    {
-        return new OreBlob(id, position, images,actionPeriod, animationPeriod);
-    }
-    
-    public static Rat createRat(String id, Point position,
-                                       int actionPeriod, int animationPeriod, List<PImage> images)
-    {
-        return new Rat(id, position, images,actionPeriod, animationPeriod);
+                                        int actionPeriod, int animationPeriod, List<PImage> images) {
+        return new OreBlob(id, position, images, actionPeriod, animationPeriod);
     }
 
-    public static Quake createQuake(Point position, List<PImage> images)
-    {
+    public static Rat createRat(String id, Point position,
+                                int actionPeriod, int animationPeriod, List<PImage> images) {
+        return new Rat(id, position, images, actionPeriod, animationPeriod);
+    }
+
+    public static Quake createQuake(Point position, List<PImage> images) {
         return new Quake(QUAKE_ID, position, images, QUAKE_ACTION_PERIOD, QUAKE_ANIMATION_PERIOD);
     }
 
     public static Vein createVein(String id, Point position, int actionPeriod,
-                                    List<PImage> images)
-    {
+                                  List<PImage> images) {
         return new Vein(id, position, images, actionPeriod);
     }
 }
