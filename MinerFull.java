d71b49a789285d0aa8d6e76af09c1edfd96a1a7a
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-17 14:04:43 -0700

    reformat

--- MinerFull.java
+++ MinerFull.java
@@ -3,34 +3,31 @@ import processing.core.PImage;
 import java.util.List;
 import java.util.Optional;
 
-public class MinerFull extends MoveAbstract{
+public class MinerFull extends MoveAbstract {
 
     private final int resourceLimit;
 
     public MinerFull(String id, Point position, List<PImage> images, int resourceLimit, int resourceCount,
-                        int actionPeriod, int animationPeriod)
-    {
+                     int actionPeriod, int animationPeriod) {
         super(id, position, images, actionPeriod, animationPeriod);
         this.resourceLimit = resourceLimit;
 
     }
-    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler)
-    {
-        Optional<Entity> fullTarget = world.findNearest(getPosition(),
-                    Blacksmith.class);
 
-        if (fullTarget.isPresent() &&
-                moveTo(world, fullTarget.get(), scheduler))
-        { transform(world, scheduler, imageStore); }
-        else {
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+        Optional<Entity> fullTarget = world.findNearest(getPosition(),
+                Blacksmith.class);
+
+        if (fullTarget.isPresent() && moveTo(world, fullTarget.get(), scheduler)) {
+            transform(world, scheduler, imageStore);
+        } else {
             scheduler.scheduleEvent(this,
                     createActivity(world, imageStore),
                     getActionPeriod());
         }
     }
 
-    private void transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore)
-    {
+    private void transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
         MinerNotFull miner = Factory.createMinerNotFull(getId(), resourceLimit,
                 getPosition(), getActionPeriod(), getAnimationPeriod(),
                 getImages());
