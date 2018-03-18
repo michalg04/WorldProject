3bebd5784f5078c155e53488920100e218e73b77
bkealoha <35231252+bkealoha@users.noreply.github.com>
2018-03-18 11:46:18 -0700

    edit cookie class
    make it act like a blacksmith or vein

--- Cookie.java
+++ Cookie.java
@@ -3,34 +3,10 @@ import processing.core.PImage;
 import java.util.List;
 import java.util.Random;
 
-public class Cookie extends ActiveAbstract{
+public class Cookie extends EntityAbstract{
 
-    private static final String BLOB_KEY = "blob";
-    private static final String BLOB_ID_SUFFIX = " -- blob";
-    private static final int BLOB_PERIOD_SCALE = 4;
-    private static final int BLOB_ANIMATION_MIN = 50;
-    private static final int BLOB_ANIMATION_MAX = 150;
-
-    public Cookie(String id, Point position, List<PImage> images, int actionPeriod)
+    public Cookie(String id, Point position, List<PImage> images)
     {
-        super(id, position, images, actionPeriod);
+        super(id, position, images);
     }
-
-    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler)
-    {
-        Point pos = getPosition();  // store current position before removing
-
-        world.removeEntity(this);
-        scheduler.unscheduleAllEvents(this);
-
-        OreBlob blob = Factory.createOreBlob(getId() + BLOB_ID_SUFFIX,
-                pos, getActionPeriod() / BLOB_PERIOD_SCALE,
-                BLOB_ANIMATION_MIN +
-                        rand.nextInt(BLOB_ANIMATION_MAX - BLOB_ANIMATION_MIN),
-                imageStore.getImageList(BLOB_KEY));
-
-        world.addEntity(blob);
-        blob.scheduleActions(scheduler, world, imageStore);
-    }
-
 }
