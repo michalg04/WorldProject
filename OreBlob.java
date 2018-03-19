6b17a57a80e4b18df1a00cb300a88d4912431d89
michalg04 <35315239+michalg04@users.noreply.github.com>
2018-03-19 15:58:56 -0700

    Update OreBlob.java

--- OreBlob.java
+++ OreBlob.java
@@ -7,6 +7,11 @@ import java.util.function.Predicate;
 
 public class OreBlob extends MoveAbstract{
     private static final String QUAKE_KEY = "quake";
+    private static final String RAT_KEY = "rat";
+    private static final String RAT_ID_SUFFIX = " -- rat";
+    private static final int RAT_PERIOD_SCALE = 4;
+    private static final int RAT_ANIMATION_MIN = 50;
+    private static final int RAT_ANIMATION_MAX = 150;
 
     public OreBlob(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod)
     {
@@ -33,33 +38,31 @@ public class OreBlob extends MoveAbstract{
             }
         }
 
-        scheduler.scheduleEvent(this,
-                createActivity(world, imageStore),
-                nextPeriod);
+        Optional<Entity> restTarget = world.findNearest(getPosition(), Restaurant.class);
+        if (restTarget.isPresent()) {
+            transform(world, scheduler, imageStore);
+        } else {
+            scheduler.scheduleEvent(this,
+                    createActivity(world, imageStore),
+                    nextPeriod);
+        }
+    }
+
+    private void transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
+        Rat rat = Factory.createRat(getId() + RAT_ID_SUFFIX,
+                getPosition(), getActionPeriod() / RAT_PERIOD_SCALE,
+                RAT_ANIMATION_MIN +
+                        rand.nextInt(RAT_ANIMATION_MAX - RAT_ANIMATION_MIN),
+                imageStore.getImageList(RAT_KEY));
+        world.removeEntity(this);
+        scheduler.unscheduleAllEvents(this);
+
+        world.addEntity(rat);
+        rat.scheduleActions(scheduler, world, imageStore);
     }
 
     public Point nextPosition(WorldModel world, Point destPos)
-    { /*
-        int horiz = Integer.signum(destPos.x - getPosition().x);
-        Point newPos = new Point(getPosition().x + horiz, getPosition().y);
-
-        Optional<Entity> occupant = world.getOccupant(newPos);
-
-        if (horiz == 0 ||
-                (occupant.isPresent() && !(occupant.get() instanceof Ore)))
-        {
-            int vert = Integer.signum(destPos.y - getPosition().y);
-            newPos = new Point(getPosition().x, getPosition().y + vert);
-            occupant = world.getOccupant(newPos);
-
-            if (vert == 0 ||
-                    (occupant.isPresent() && !(occupant.get() instanceof Ore)))
-            {
-                newPos = getPosition();
-            }
-        }
-        return newPos;
-        */
+    { 
         int horiz = Integer.signum(destPos.x - getPosition().x);
         Point newPos = new Point(getPosition().x + horiz, getPosition().y);
 
